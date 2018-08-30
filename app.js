84fbb4ecd5f122d6d7811438dff4349b6845dfc0
Connor Cahill <connor@Connors-MacBook.local>
2018-08-30 10:55:16 -0700

    users can see all reviews

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ var exhbs = require('express-handlebars');
 app.engine('handlebars',exhbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
+/*
 app.get('/', (req, res) => {
     res.render('home', {msg: 'Hello World!'});
+}) */
+
+//mock array of projects
+let reviews = [
+    {title: 'Great Review'},
+    {title: 'Next Review'}
+]
+
+//index//
+app.get('/', (req, res) => {
+    res.render('reviews-index', {reviews: reviews});
 })
+
 
 
 
