e96d64d119d5267135a444412876b2ed283ae467
Connor Cahill <connor@Connors-MacBook.local>
2018-09-04 10:17:25 -0700

    comments now visible, but only in terminal

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,8 +1,13 @@
 
 module.exports = function(app) {
     ////// NEW COMMENT ///////
-    app.post('/reviews/comments', (req, res) {
-        res.send('reviews comment');
+    app.post('/reviews/comments', (req, res) => {
+        //res.send('reviews comment');
+        Comment.create(req.body).then(comment => {
+            res.redirect(`/reviews/${comment.reviewId}`)
+        }).catch((err) => {
+            console.log(err.message);
+        })
     })
 
 }
--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Comment = mongoose.model('Comment', {
     title: String,
     content: String
+    reviewId: {type: Schema.Types.ObjectId, ref: 'Review'}
 });
 
 module.exports = Comment
